8575c645f301452960320f817424f1944e393315
kooza-colline <koozacollinz1@gmail.com>
2024-08-23 11:25:52 +0300

    updated the ui

--- app/page.tsx
+++ app/page.tsx
@@ -6,36 +6,46 @@ export default function Page() {
   return (
     <div className="flex flex-col min-h-[100dvh] ">
       <main className="flex-1 ">
-      <section className="relative w-full py-28 md:py-24 lg:py-28  xl:py-38 overflow-hidden bg-3">
-  {/* <div className="absolute inset-0 bg-gradient-to-r from-gray-800 to-transparent pointer-events-none"></div> */}
-  {/* <div className="absolute inset-0 bg-cover bg-center z-0" style={{ backgroundImage: "url('pos-5.jpg')", opacity: "0.3" }}></div> */}
-  
+      <section className="relative w-full py-28 md:py-24 lg:py-28  xl:py-38 overflow-hidden ">
   <div className="container relative z-10 px-4 md:px-6">
-    <div className="flex flex-col items-center space-y-4 text-center">
-      <div className="space-y-5">
-        <h1 className="text-3xl leading-10 font-black tracking-normal sm:text-4xl md:text-5xl lg:text-5xl/none text-gray-900 dark:text-white lg:w-[1000px]">
+  <div className="flex flex-col items-center space-y-4 text-center">
+    <div className="space-y-5">
+      <h1 className="text-3xl leading-10 font-black tracking-normal sm:text-4xl md:text-5xl lg:text-5xl/none text-gray-900 dark:text-white lg:w-[1000px]">
         Paint and Sip Uganda Inventory Management
-        </h1>
-        <p className="mx-auto max-w-[800px] font-medium dark:font-medium text-gray-800 md:text-lg dark:text-gray-200 text-sm ">
+      </h1>
+      <p className="mx-auto max-w-[800px] font-medium dark:font-medium text-gray-800 md:text-lg dark:text-gray-200 text-sm">
         Automate inventory and sales with precision. Ensuring accurate tracking and reduce discrepancies for a seamless experience.
-        </p>
-      </div>
-      <div className="space-x-5">
-        <Link
-          className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-8 py-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-          href="/contact"
-        >
-          Contact us
-        </Link>
-        <Link
-          className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 py-5 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-          href="/dashboard/order-line"
-        >
-         Get Started
-        </Link>
-      </div>
+      </p>
+    </div>
+    <div className="space-x-5">
+      <Link
+        className="inline-flex h-9 items-center justify-center rounded-md bg-gray-900 px-8 py-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+        href="/contact"
+      >
+        Contact us
+      </Link>
+      <Link
+        className="inline-flex h-9 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 py-5 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+        href="/dashboard/order-line"
+      >
+        Get Started
+      </Link>
     </div>
   </div>
+  <div
+    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+    aria-hidden="true"
+  >
+    <div
+      className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+      style={{
+        clipPath:
+          "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+      }}
+    ></div>
+  </div>
+</div>
+
 </section>
 
 
